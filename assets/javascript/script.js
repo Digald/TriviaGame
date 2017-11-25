758d02adc54fb491de396f391183a7582c96a6a8
Digald <nosracer_gt@sbcglobal.net>
2017-11-24 20:25:29 -0600

    set up templates for each screen

---
+++ assets/javascript/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+// varibles for each screen 
+var startGame = $("#startGame");
+var gameForm = $("#multipleChoice");
+var transitionPage = $("#transitionPage");
+var endScreen = $("#endScreen");
+
+// turn different screens on or off for development
+startGame.hide();
+gameForm.hide();
+transitionPage.show();
+endScreen.hide();
+
+// 
+
+
+
+
+});
